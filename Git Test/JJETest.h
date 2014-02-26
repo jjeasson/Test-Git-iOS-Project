7a839201ae9c92c1a0f368d4351321fba58f806c
Joseph J Easson <jje1@jje-imac-27-i7.home>
2014-02-26 13:44:20 +0000

    added a test class

---
+++ Git Test/JJETest.h
@@ -0,0 +1,13 @@
+//
+//  JJETest.h
+//  Git Test
+//
+//  Created by Joseph J Easson on 26/02/2014.
+//  Copyright (c) 2014 Joseph J Easson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JJETest : NSObject
+
+@end
---
+++ Git Test/JJETest.m
@@ -0,0 +1,13 @@
+//
+//  JJETest.m
+//  Git Test
+//
+//  Created by Joseph J Easson on 26/02/2014.
+//  Copyright (c) 2014 Joseph J Easson. All rights reserved.
+//
+
+#import "JJETest.h"
+
+@implementation JJETest
+
+@end
