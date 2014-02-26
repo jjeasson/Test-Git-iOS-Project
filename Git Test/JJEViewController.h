95f06432ab83d08dd570e3be6ce4201cd20e0be1
Joseph J Easson <jje1@jje-imac-27-i7.home>
2014-02-26 13:27:09 +0000

    Initial Commit

---
+++ Git Test/JJEViewController.h
@@ -0,0 +1,13 @@
+//
+//  JJEViewController.h
+//  Git Test
+//
+//  Created by Joseph J Easson on 26/02/2014.
+//  Copyright (c) 2014 Joseph J Easson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJEViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Joseph J Easson on 26/02/2014.
+//  Copyright (c) 2014 Joseph J Easson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JJEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JJEAppDelegate class]));
+    }
+}
